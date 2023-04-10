8b0fa89ef09edfba8a1c814cddf2b2859649e420
miltongg <miltongonzalezgaliano@gmail.com>
2023-04-09 22:02:24 -0400

    Added createReply endpoint, Reply model

---
+++ client/src/helpers/customStyles.ts
@@ -0,0 +1,22 @@
+export const doneButtonStyle = {
+  color: 'orange',
+  '&:hover': {
+    color: 'red',
+    cursor: 'pointer'
+  },
+  '&:active': {
+    color: 'black'
+  }
+}
+
+export const editButtonStyle = {
+  color: 'orange',
+  ml: 1,
+  '&:hover': {
+    color: 'red',
+    cursor: 'pointer'
+  },
+  '&:active': {
+    color: 'block'
+  }
+}
